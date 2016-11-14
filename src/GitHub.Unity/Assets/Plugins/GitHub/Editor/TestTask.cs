603b0ea87fc6b80940d36daa7d50fd3883dc6ffa
Emil "AngryAnt" Johansen <git@eej.dk>
2016-11-14 18:24:19 +0100

    Test task. Available via Unity menu Assets -> GitHub.

---
+++ src/GitHub.Unity/Assets/Plugins/GitHub/Editor/TestTask.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEditor;
+using System.Threading;
+using System;
+
+
+namespace GitHub
+{
+	class TestTask : ITask
+	{
+		[MenuItem ("Assets/GitHub/Test Blocking Critical")]
+		static void TestA ()
+		{
+			Test (new TestTask (true));
+		}
+
+
+		[MenuItem ("Assets/GitHub/Test Non-blocking Critical")]
+		static void TestB ()
+		{
+			Test (new TestTask (false));
+		}
+
+
+		static void Test (TestTask task)
+		{
+			EditorApplication.delayCall += () => Tasks.Add (task);
+		}
+
+
+		TestTask (bool shouldBlock)
+		{
+			Blocking = shouldBlock;
+			Done = false;
+			Progress = 0.0f;
+		}
+
+
+		public bool Blocking {get; protected set;}
+		public float Progress {get; protected set;}
+		public bool Done {get; protected set;}
+		public bool Queued {get {return true;}}
+		public bool Critical {get {return true;}}
+		public bool Cached {get {return true;}}
+		public Action<ITask> OnEnd {get;set;}
+		public string Label { get { return "Test task"; } }
+
+
+		public void Run ()
+		{
+			Debug.LogFormat ("{0} start", Label);
+
+			Done = false;
+			Progress = 0.0f;
+
+			const int
+				kSteps = 10,
+				kStepSleep = 1000;
+
+			for (int step = 0; !Done && step < kSteps; ++step)
+			{
+				Progress = step / (float)kSteps;
+				Thread.Sleep (kStepSleep);
+			}
+
+			Progress = 1.0f;
+			Done = true;
+
+			Debug.LogFormat ("{0} end", Label);
+
+			if (OnEnd != null)
+			{
+				OnEnd (this);
+			}
+		}
+
+
+		public void Abort ()
+		{
+			Debug.LogFormat ("Aborting {0}", Label);
+
+			Done = true;
+		}
+	}
+}
