8fde6e3f8258a4e346f1b964d81b7a6845fb2b5a
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-01-25 20:04:00 -0500

    Using EntryPoint's Initialize method to configure public instances of several "services"

--- src/UnityExtension/Assets/Editor/GitHub.Unity/Misc/EntryPoint.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/Misc/EntryPoint.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using ILogger = GitHub.Unity.Logging.ILogger;
 
 namespace GitHub.Unity
 {
     [InitializeOnLoad]
     class EntryPoint
     {
+        private static readonly ILogger logger = Logging.Logger.GetLogger<EntryPoint>();
+
         // this may run on the loader thread if it's an appdomain restart
         static EntryPoint()
         {
-            Logging.Logger.Debug("Entry Point");
             EditorApplication.update += Initialize;
         }
 
         // we do this so we're guaranteed to run on the main thread, not the loader thread
         private static void Initialize()
         {
+            logger.Debug("Initialize");
+
             EditorApplication.update -= Initialize;
 
             Tasks.Initialize();
@@ -28,7 +32,23 @@ namespace GitHub.Unity
 
             ProjectWindowInterface.Initialize();
 
+            FileSystem = new FileSystem();
+
+            Environment = new DefaultEnvironment();
+
+            GitEnvironment = Environment.IsWindows
+                ? (IGitEnvironment) new WindowsGitEnvironment(FileSystem, Environment)
+                : new LinuxBasedGitEnvironment(FileSystem, Environment);
+
+            ProcessManager = new ProcessManager(Environment, GitEnvironment, FileSystem);
+
             Window.Initialize();
         }
+
+        public static IEnvironment Environment { get; private set; }
+        public static IGitEnvironment GitEnvironment { get; private set; }
+
+        public static IFileSystem FileSystem { get; private set; }
+        public static IProcessManager ProcessManager { get; private set; }
     }
 }
