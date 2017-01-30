88cdb1076789b48f2a6bfaa0526eceff08f21138
Andreia Gaita <shana@spoiledcat.net>
2017-01-30 10:30:48 +0100

    wip Login view

---
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/Services/AuthenticationService.cs
@@ -0,0 +1,76 @@
+﻿using GitHub.Api;
+using GitHub.Models;
+using GitHub.Primitives;
+using Octokit;
+using System;
+using System.Reflection;
+
+namespace GitHub.Unity
+{
+
+    public static class ApplicationInfo
+    {
+#if DEBUG
+        public const string ApplicationName = "Unity123";
+        public const string ApplicationProvider = "GitHub";
+#else
+        public const string ApplicationName = "Unity123";
+        public const string ApplicationProvider = "GitHub";
+#endif
+        public const string ApplicationSafeName = "Unity123";
+        public const string ApplicationDescription = "GitHub for Unity";
+    }
+
+    public class Program : IProgram
+    {
+        public Program()
+        {
+            applicationName = ApplicationInfo.ApplicationName;
+            applicationDescription = ApplicationInfo.ApplicationDescription;
+
+            var executingAssembly = typeof(Program).Assembly;
+            AssemblyName = executingAssembly.GetName();
+            ProductHeader = new ProductHeaderValue(ApplicationInfo.ApplicationSafeName, AssemblyName.Version.ToString());
+        }
+
+        readonly string applicationName;
+        readonly string applicationDescription;
+
+        /// <summary>
+        /// Name of this application
+        /// </summary>
+        public string ApplicationName { get { return applicationName; } }
+
+        /// <summary>
+        /// Name of this application
+        /// </summary>
+        public string ApplicationDescription { get { return applicationDescription; } }
+
+        /// <summary>
+        /// The currently executing assembly.
+        /// </summary>
+        public AssemblyName AssemblyName { get; private set; }
+
+        /// <summary>
+        /// The product header used in the user agent.
+        /// </summary>
+        public ProductHeaderValue ProductHeader { get; private set; }
+    }
+
+    class AuthenticationService
+    {
+        public enum LoginResult
+        {
+            Ok,
+            TwoFA,
+            Failed
+        }
+
+        public void Login(Action<LoginResult> callback)
+        {
+            var api = new SimpleApiClientFactory(new Program());
+            var client = api.Create(new UriString("https://github.com/github/UnityInternal"));
+            var repo = client.GetRepository()
+        }
+    }
+}
---
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/UI/AuthenticationView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace GitHub.Unity
+{
+    [Serializable]
+    class AuthenticationView : Subview
+    {
+        const string serverLabel = "Server";
+        const string usernameLabel = "Username";
+        const string passwordLabel = "Password";
+        const string twofaLabel = "Code";
+        const string loginButton = "Login";
+
+        [SerializeField] private Vector2 scroll;
+        [SerializeField] private string username = "";
+        [SerializeField] private string password = "";
+        [SerializeField] private string[] two2fa = new string[6];
+
+        public override void OnGUI()
+        {
+            scroll = GUILayout.BeginScrollView(scroll);
+            {
+                GUILayout.BeginVertical();
+                {
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label(usernameLabel);
+                        username = GUILayout.TextField(username);
+                    }
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label(passwordLabel);
+                        password = GUILayout.TextField(password);
+                    }
+                    GUILayout.EndHorizontal();
+                    if (GUILayout.Button(loginButton))
+                    {
+                        var serv = new AuthenticationService();
+                        serv.Login(null);
+                    }
+                }
+            }
+            GUILayout.EndScrollView();
+        }
+    }
+}
