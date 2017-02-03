4c198c9b8673b1a7f675a902da1b43e8146feae5
Andreia Gaita <shana@spoiledcat.net>
2017-02-03 18:31:56 +0100

    Copy dlls to project dir so Unity can load them
    If we're letting Unity build GitHub.Unity, then it needs all the dependent DLLs so it can include them in the compiled references, and they're going somewhere else, sooooo copy them here. We might want to invert this and copy them over to the Test project instead, but [insert shrug meme here]
    
    Also ignore s**t

---
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/post-build-copy.cmd
@@ -0,0 +1,10 @@
+echo rmdir /s %1\obj
+cp %2\Mono.Security.dll %1
+cp %2\GitHub.Api.dll %1
+cp %2\Octokit.dll %1
+cp %2\Octokit-Extensions35.dll %1
+cp %2\AsyncBridge.Net35.dll %1
+cp %2\Rackspace.Threading.dll %1
+cp %2\ReadOnlyCollectionsInterfaces.dll %1
+cp %2\System.Net.Http.dll %1
+cp %2\System.Threading.dll %1
