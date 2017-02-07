ead5b479663cd509a45aad7e4e0cc8011b022dc5
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-02-07 10:27:32 -0500

    Adding git remote add/remove commands

---
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/Tasks/GitRemoteAddTask.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+
+namespace GitHub.Unity
+{
+    class GitRemoteAddTask : GitTask
+    {
+        private readonly string name;
+        private readonly string url;
+
+        private GitRemoteAddTask(string name, string url, Action onSuccess, Action onFailure)
+            : base(str => onSuccess.SafeInvoke(), onFailure)
+        {
+            this.name = name;
+            this.url = url;
+        }
+
+        public static void Schedule(string name, string url, Action onSuccess, Action onFailure = null)
+        {
+            Tasks.Add(new GitRemoteAddTask(name, url, onSuccess, onFailure));
+        }
+
+        public override bool Blocking
+        {
+            get { return false; }
+        }
+
+        public override TaskQueueSetting Queued
+        {
+            get { return TaskQueueSetting.Queue; }
+        }
+
+        public override bool Critical
+        {
+            get { return false; }
+        }
+
+        public override bool Cached
+        {
+            get { return true; }
+        }
+
+        public override string Label
+        {
+            get { return "git remote"; }
+        }
+
+        protected override string ProcessArguments
+        {
+            get { return String.Format("remote add {0} {1}", name, url); }
+        }
+    }
+}
---
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/Tasks/GitRemoteRemoveTask.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace GitHub.Unity
+{
+    class GitRemoteRemoveTask : GitTask
+    {
+        private readonly string name;
+
+        private GitRemoteRemoveTask(string name, Action onSuccess, Action onFailure)
+            : base(str => onSuccess.SafeInvoke(), onFailure)
+        {
+            this.name = name;
+        }
+
+        public static void Schedule(string name, Action onSuccess, Action onFailure = null)
+        {
+            Tasks.Add(new GitRemoteRemoveTask(name, onSuccess, onFailure));
+        }
+
+        public override bool Blocking
+        {
+            get { return false; }
+        }
+
+        public override TaskQueueSetting Queued
+        {
+            get { return TaskQueueSetting.Queue; }
+        }
+
+        public override bool Critical
+        {
+            get { return false; }
+        }
+
+        public override bool Cached
+        {
+            get { return true; }
+        }
+
+        public override string Label
+        {
+            get { return "git remote"; }
+        }
+
+        protected override string ProcessArguments
+        {
+            get { return String.Format("remote remove {0}", name); }
+        }
+    }
+}
