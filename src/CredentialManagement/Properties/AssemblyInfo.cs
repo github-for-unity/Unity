28b6d66b11f9eb6b67e18166d4512df56765bbbe
Andreia Gaita <shana@spoiledcat.net>
2017-02-07 23:57:25 +0100

    We don't need this right now

--- src/CredentialManagement/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GitHub.CredentialManagement")]
-[assembly: AssemblyDescription("GitHub CredentialManagement")]
-[assembly: Guid("41a47c5b-c606-45b4-b83c-22b9239e4da0")]
