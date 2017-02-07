4a3f97fe375f141605c75429dc87776a589b0c37
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-02-07 11:28:37 -0500

    Adding GitInitTask

---
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/Tasks/GitInitTask.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace GitHub.Unity
+{
+    class GitInitTask : GitTask
+    {
+        private GitInitTask(Action onSuccess, Action onFailure)
+            : base(str => onSuccess.SafeInvoke(), onFailure)
+        {
+        }
+
+        public static void Schedule(Action onSuccess, Action onFailure = null)
+        {
+            Tasks.Add(new GitInitTask(onSuccess, onFailure));
+        }
+
+        public override bool Blocking
+        {
+            get { return false; }
+        }
+
+        public override TaskQueueSetting Queued
+        {
+            get { return TaskQueueSetting.Queue; }
+        }
+
+        public override bool Critical
+        {
+            get { return false; }
+        }
+
+        public override bool Cached
+        {
+            get { return true; }
+        }
+
+        public override string Label
+        {
+            get { return "git init"; }
+        }
+
+        protected override string ProcessArguments
+        {
+            get { return "init"; }
+        }
+    }
+}
