0ac29bc637bbd04329bee4471e04c03bbcfc94de
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-02-14 17:44:02 -0500

    Removing base class and interface

--- src/GitHub.Api/Installer/IPortableGitManager.cs
+++ src/GitHub.Api/Installer/IPortableGitManager.cs
@@ -1,11 +1,14 @@
 namespace GitHub.Api
 {
-    interface IPortableGitManager: IPortablePackageManager
+    interface IPortableGitManager
     {
         void ExtractGitIfNeeded();
         bool IsExtracted();
         string GetPortableGitDestinationDirectory(bool createIfNeeded = false);
         void EnsureSystemConfigFileExtracted();
         string ExtractSuggestedGitAttributes(string targetDirectory);
+        bool IsPackageExtracted();
+        string GetPackageDestinationDirectory(bool createIfNeeded = false, bool includeExpectedVersion = true);
+        string GetPackageNameWithVersion();
     }
 }
