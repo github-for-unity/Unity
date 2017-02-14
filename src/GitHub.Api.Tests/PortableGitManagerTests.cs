02719aa078d07a101be2ed2f913830c76f5358b5
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-02-14 16:11:05 -0500

    Completed ShouldExtractGitIfNeeded

--- src/GitHub.Api.Tests/PortableGitManagerTests.cs
+++ src/GitHub.Api.Tests/PortableGitManagerTests.cs
@@ -10,6 +10,9 @@ namespace GitHub.Unity.Tests
     [TestFixture]
     public class PortableGitManagerTests
     {
+        private const string ExtensionFolder = @"c:\ExtensionFolder";
+        private const string WindowsPortableGitZip = ExtensionFolder + @"\resources\windows\PortableGit.zip";
+
         private IEnvironment CreateEnvironment(string extensionfolder)
         {
             var environment = Substitute.For<IEnvironment>();
@@ -17,13 +20,12 @@ namespace GitHub.Unity.Tests
             return environment;
         }
 
-        private IFileSystem CreateFileSystem(string[] thatExist, Dictionary<string, string[]> dictionary)
+        private IFileSystem CreateFileSystem(string[] thatExist, Dictionary<string, string[]> dictionary, string[] randomFileNames)
         {
             var fileSystem = Substitute.For<IFileSystem>();
             var realFileSystem = new FileSystem();
 
-            fileSystem.Combine(Arg.Any<string>(), Arg.Any<string>()).Returns(info =>
-            {
+            fileSystem.Combine(Arg.Any<string>(), Arg.Any<string>()).Returns(info => {
                 var path1 = (string)info[0];
                 var path2 = (string)info[1];
                 var combine = realFileSystem.Combine(path1, path2);
@@ -31,8 +33,7 @@ namespace GitHub.Unity.Tests
                 return combine;
             });
 
-            fileSystem.Combine(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(info =>
-            {
+            fileSystem.Combine(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(info => {
                 var path1 = (string)info[0];
                 var path2 = (string)info[1];
                 var path3 = (string)info[2];
@@ -41,16 +42,14 @@ namespace GitHub.Unity.Tests
                 return combine;
             });
 
-            fileSystem.FileExists(Arg.Any<string>()).Returns(info =>
-            {
+            fileSystem.FileExists(Arg.Any<string>()).Returns(info => {
                 var path1 = (string)info[0];
                 var result = thatExist.Contains(path1);
                 Logger.Debug(@"FileSystem.FileExists(""{0}"") -> {1}", path1, result);
                 return result;
             });
 
-            fileSystem.ReadAllText(Arg.Any<string>()).Returns(info =>
-            {
+            fileSystem.ReadAllText(Arg.Any<string>()).Returns(info => {
                 var path1 = (string)info[0];
 
                 string result = null;
@@ -71,15 +70,24 @@ namespace GitHub.Unity.Tests
                 return result;
             });
 
-            var archiveFolderName = realFileSystem.GetRandomFileName();
-            fileSystem.GetRandomFileName().Returns(info => archiveFolderName);
+            var randomFileIndex = 0;
+
+            fileSystem.GetRandomFileName().Returns(info => {
+                var result = randomFileNames[randomFileIndex];
+
+                randomFileIndex++;
+                randomFileIndex = randomFileIndex % randomFileNames.Length;
+
+                Logger.Debug(@"FileSystem.GetRandomFileName() -> {0}", result);
+
+                return result;
+            });
             return fileSystem;
         }
 
         private ISharpZipLibHelper CreateSharpZipLibHelper()
         {
-            var sharpZipLibHelper = Substitute.For<ISharpZipLibHelper>();
-            return sharpZipLibHelper;
+            return Substitute.For<ISharpZipLibHelper>();
         }
 
         [OneTimeSetUp]
@@ -92,32 +100,57 @@ namespace GitHub.Unity.Tests
         public ILogging Logger { get; set; }
 
         [Test]
-        public void ShouldFindExistingInstallation()
+        public void ShouldExtractGitIfNeeded()
         {
-            const string extensionfolder = @"c:\ExtensionFolder";
+            var environment = CreateEnvironment(ExtensionFolder);
+            var sharpZipLibHelper = CreateSharpZipLibHelper();
 
-            var environment = CreateEnvironment(extensionfolder);
+            var filesThatExist = new[] { ExtensionFolder + @"\resources\windows\PortableGit.zip", };
+
+            var fileContents = new Dictionary<string, string[]> { };
+
+            var randomFileNames = new string[] {
+                "randomFolder1",
+                "randomFolder2",
+            };
+
+            var fileSystem = CreateFileSystem(filesThatExist, fileContents, randomFileNames);
+
+            var portableGitManager = new PortableGitManager(environment, fileSystem, sharpZipLibHelper);
+            portableGitManager.ExtractGitIfNeeded();
+
+            const string shouldExtractTo = @"c:\ExtensionFolder\randomFolder1.deleteme";
+            sharpZipLibHelper.Received().ExtractZipFile(WindowsPortableGitZip, shouldExtractTo);
+        }
+
+        [Test]
+        public void ShouldNotExtractGitIfNotNeeded()
+        {
+            var environment = CreateEnvironment(ExtensionFolder);
             var sharpZipLibHelper = CreateSharpZipLibHelper();
 
             var filesThatExist = new[] {
-                extensionfolder + @"\resources\windows\PortableGit.zip",
-                extensionfolder + @"\PortableGit_f02737a78695063deace08e96d5042710d3e32db\cmd\git.exe",
-                extensionfolder + @"\PortableGit_f02737a78695063deace08e96d5042710d3e32db\VERSION"
+                WindowsPortableGitZip,
+                ExtensionFolder + @"\PortableGit_f02737a78695063deace08e96d5042710d3e32db\cmd\git.exe",
+                ExtensionFolder + @"\PortableGit_f02737a78695063deace08e96d5042710d3e32db\VERSION"
             };
 
             var fileContents = new Dictionary<string, string[]> {
                 {
-                    extensionfolder + @"\PortableGit_f02737a78695063deace08e96d5042710d3e32db\VERSION",
-                    new [] {
-                        "f02737a78695063deace08e96d5042710d3e32db"
-                    }
+                    ExtensionFolder + @"\PortableGit_f02737a78695063deace08e96d5042710d3e32db\VERSION",
+                    new[] { "f02737a78695063deace08e96d5042710d3e32db" }
                 }
             };
 
-            var fileSystem = CreateFileSystem(filesThatExist, fileContents);
+            var randomFileNames = new string[] {
+            };
+
+            var fileSystem = CreateFileSystem(filesThatExist, fileContents, randomFileNames);
 
             var portableGitManager = new PortableGitManager(environment, fileSystem, sharpZipLibHelper);
             portableGitManager.ExtractGitIfNeeded();
+
+            sharpZipLibHelper.DidNotReceiveWithAnyArgs().ExtractZipFile(Arg.Any<string>(), Arg.Any<string>());
         }
     }
 }
