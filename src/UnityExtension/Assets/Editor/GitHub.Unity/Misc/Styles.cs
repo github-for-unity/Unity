c6723bee7fa633ef365caed9cafb23c267cc108a
Don Okuda <don.okuda@gmail.com>
2017-02-23 11:25:06 -0800

    Don't do a negative indentation for tree root

--- src/UnityExtension/Assets/Editor/GitHub.Unity/Misc/Styles.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/Misc/Styles.cs
@@ -29,7 +29,7 @@ namespace GitHub.Unity
             FoldoutWidth = 11f,
             FoldoutIndentation = -2f,
             TreeIndentation = 17f,
-            TreeRootIndentation = -5f,
+            TreeRootIndentation = 5f,
             TreeVerticalSpacing = 3f,
             CommitIconSize = 16f,
             CommitIconHorizontalPadding = -5f,
