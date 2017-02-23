1f9eb23813832bd773b83d65c8d7875a838c2183
Andreia Gaita <shana@spoiledcat.net>
2017-02-22 16:50:55 -0800

    Enable source control icons in the project window

--- src/UnityExtension/Assets/Editor/GitHub.Unity/Misc/ProjectWindowInterface.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/Misc/ProjectWindowInterface.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 
@@ -14,7 +15,7 @@ namespace GitHub.Unity
         {
             EditorApplication.projectWindowItemOnGUI -= OnProjectWindowItemGUI;
             EditorApplication.projectWindowItemOnGUI += OnProjectWindowItemGUI;
-            repository.OnRepositoryChanged += OnStatusUpdate;
+            repository.OnRepositoryChanged += status => Tasks.ScheduleMainThread(() => OnStatusUpdate(status));
             initialized = true;
         }
 
