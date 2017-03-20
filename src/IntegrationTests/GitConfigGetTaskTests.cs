9f389918773eaf0e6ffd3142150aed1856442e02
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-03-20 09:07:56 -0400

    Trying to understand why the pull integration test fails

---
+++ src/IntegrationTests/GitConfigGetTaskTests.cs
@@ -0,0 +1,30 @@
+using System.Threading;
+using GitHub.Unity;
+using NCrunch.Framework;
+using NUnit.Framework;
+
+namespace IntegrationTests
+{
+    [TestFixture]
+    class GitConfigGetTaskTests : BaseGitEnvironmentTest
+    {
+        [Test, Isolated]
+        public void ShouldWork()
+        {
+            var en = new WindowsGitEnvironment(Environment, FileSystem);
+            var processManager = new ProcessManager(Environment, en, CancellationToken.None);
+
+            string result = null;
+
+            var gitConfigGetTask = new GitConfigGetTask(Environment, processManager, new TaskResultDispatcher<string>(
+                s =>
+                {
+                    result = s;
+                }), "credential.helper", GitConfigSource.NonSpecified);
+
+            gitConfigGetTask.Run(CancellationToken.None);
+
+            Logger.Trace("ShouldWork: {0}", result);
+        }
+    }
+}
