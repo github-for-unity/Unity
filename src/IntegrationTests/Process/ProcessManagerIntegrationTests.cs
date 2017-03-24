e4c616a5a7348dc4e786103a393a00c86953dd69
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-03-24 14:46:52 -0400

    Fixing ProcessManagerIntegrationTests

--- src/IntegrationTests/Process/ProcessManagerIntegrationTests.cs
+++ src/IntegrationTests/Process/ProcessManagerIntegrationTests.cs
@@ -22,7 +22,9 @@ namespace IntegrationTests
 
             var platform = new Platform(Environment, FileSystem, new TestUIDispatcher());
             GitEnvironment = platform.GitEnvironment;
+
             ProcessManager = new ProcessManager(Environment, GitEnvironment);
+            platform.Initialize(ProcessManager);
 
             Environment.UnityProjectPath = TestBasePath;
             Environment.GitExecutablePath = GitEnvironment.FindGitInstallationPath(ProcessManager).Result;
