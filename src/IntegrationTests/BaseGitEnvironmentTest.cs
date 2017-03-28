88e4a8e9d424c102cde76460d4bb4e2fb2b26393
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-03-28 16:12:13 -0400

    Fixing integration test

--- src/IntegrationTests/BaseGitEnvironmentTest.cs
+++ src/IntegrationTests/BaseGitEnvironmentTest.cs
@@ -9,7 +9,8 @@ namespace IntegrationTests
         {
             base.OnSetup();
 
-            Environment = new IntegrationTestEnvironment {
+            Environment = new IntegrationTestEnvironment
+            {
                 RepositoryPath = TestRepoPath,
                 UnityProjectPath = TestRepoPath
             };
@@ -18,6 +19,30 @@ namespace IntegrationTests
             gitSetup.SetupIfNeeded().Wait();
 
             Environment.GitExecutablePath = gitSetup.GitExecutablePath;
+
+            FileSystem.SetCurrentDirectory(TestRepoPath);
+
+            Platform = new Platform(Environment, FileSystem, new TestUIDispatcher());
+            GitEnvironment = Platform.GitEnvironment;
+            ProcessManager = new ProcessManager(Environment, GitEnvironment);
+            Platform.Initialize(ProcessManager);
+
+            Environment.UnityProjectPath = TestRepoPath;
+            Environment.GitExecutablePath = GitEnvironment.FindGitInstallationPath(ProcessManager).Result;
+
+            var repositoryManagerFactory = new RepositoryManagerFactory();
+            var repositoryManager = repositoryManagerFactory.CreateRepositoryManager(Platform, TestRepoPath,
+                CancellationToken.None);
+
+            Environment.Repository = repositoryManager.Repository;
         }
+
+        public IEnvironment Environment { get; private set; }
+
+        protected Platform Platform { get; private set; }
+
+        protected ProcessManager ProcessManager { get; private set; }
+
+        protected IProcessEnvironment GitEnvironment { get; private set; }
     }
 }
--- src/IntegrationTests/BaseGitRepoTest.cs
+++ src/IntegrationTests/BaseGitRepoTest.cs
@@ -14,47 +14,14 @@ namespace IntegrationTests
 
             TestRepoPath = TestBasePath.Combine("IOTestsRepo");
 
-            Environment = new IntegrationTestEnvironment {
-                RepositoryPath = TestRepoPath
-            };
-
-            var gitSetup = new GitSetup(Environment, CancellationToken.None);
-            gitSetup.SetupIfNeeded().Wait();
-
-            Environment.GitExecutablePath = gitSetup.GitExecutablePath;
-
-            FileSystem.SetCurrentDirectory(TestRepoPath);
-
-            Platform = new Platform(Environment, FileSystem, new TestUIDispatcher());
-            GitEnvironment = Platform.GitEnvironment;
-            ProcessManager = new ProcessManager(Environment, GitEnvironment);
-            Platform.Initialize(ProcessManager);
-
-            Environment.UnityProjectPath = TestRepoPath;
-            Environment.GitExecutablePath = GitEnvironment.FindGitInstallationPath(ProcessManager).Result;
-
             using (var zipFile = new ZipFile(TestZipFilePath))
             {
                 zipFile.ExtractAll(TestBasePath.ToString(), ExtractExistingFileAction.OverwriteSilently);
             }
-
-            var repositoryManagerFactory = new RepositoryManagerFactory();
-            var repositoryManager = repositoryManagerFactory.CreateRepositoryManager(Platform, TestRepoPath,
-                CancellationToken.None);
-
-            Environment.Repository = repositoryManager.Repository;
         }
-
-        public IEnvironment Environment { get; protected set; }
-
+        
         protected NPath TestRepoPath { get; private set; }
 
-        protected Platform Platform { get; private set; }
-
         private static string TestZipFilePath => Path.Combine(SolutionDirectory, "IOTestsRepo.zip");
-
-        protected ProcessManager ProcessManager { get; private set; }
-
-        protected IProcessEnvironment GitEnvironment { get; private set; }
     }
 }
