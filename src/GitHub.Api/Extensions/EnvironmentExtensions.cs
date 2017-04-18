20d95b45aecdad3c6688efac31b601d86695fc6b
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-04-18 10:14:31 -0400

    Providing functionality to change asset path to repository path

---
+++ src/GitHub.Api/Extensions/EnvironmentExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GitHub.Unity
+{
+    static class EnvironmentExtensions
+    {
+        public static string GetRepositoryPath(this IEnvironment environment, string assetPath)
+        {
+            Guard.ArgumentNotNull(assetPath, nameof(assetPath));
+            if (environment.UnityProjectPath == environment.RepositoryPath)
+            {
+                return assetPath;
+            }
+
+            var projectPath = environment.UnityProjectPath.ToNPath();
+            var repositoryPath = environment.RepositoryPath.ToNPath();
+
+            if (repositoryPath.IsChildOf(projectPath))
+            {
+                throw new Exception($"RepositoryPath:\"{projectPath}\" should not be child or ProjectPath:\"{repositoryPath}\"");
+            }
+
+            return projectPath.RelativeTo(repositoryPath).Combine(assetPath).ToString();
+        }
+    }
+}
