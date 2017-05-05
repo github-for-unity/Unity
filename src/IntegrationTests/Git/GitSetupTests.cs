40dcc34f7e37e4d0dd8a80ab776eb6a3e1496f74
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-05-05 18:34:07 -0400

    Fixing my mistakes

--- src/IntegrationTests/Git/GitSetupTests.cs
+++ src/IntegrationTests/Git/GitSetupTests.cs
@@ -14,12 +14,7 @@ namespace IntegrationTests
         {
             var environmentPath = NPath.CreateTempDirectory("integration-test-environment");
             var environment = new IntegrationTestEnvironment(SolutionDirectory, environmentPath);
-
-            var platform = new Platform(environment, FileSystem, new TestUIDispatcher());
-            var gitEnvironment = platform.GitEnvironment;
-            var processManager = new ProcessManager(environment, gitEnvironment);
-
-            var gitSetup = new GitSetup(environment, processManager, FileSystem, CancellationToken.None);
+            var gitSetup = new GitSetup(environment, FileSystem, CancellationToken.None);
             var expectedPath = gitSetup.GitInstallationPath;
 
             var setupDone = false;
@@ -55,6 +50,10 @@ namespace IntegrationTests
                 environment.GitExecutablePath = "/usr/local/bin/git";
                 setupDone.Should().BeFalse ();
             }
+
+            var platform = new Platform(environment, FileSystem, new TestUIDispatcher());
+            var gitEnvironment = platform.GitEnvironment;
+            var processManager = new ProcessManager(environment, gitEnvironment);
 
             var gitBranches = processManager.GetGitBranches(TestRepoMasterDirtyUnsynchronized, environment.GitExecutablePath);
 
