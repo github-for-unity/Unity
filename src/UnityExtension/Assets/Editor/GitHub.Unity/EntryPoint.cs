e2a436073bbbe78c93dcdc887339102706424672
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-05-11 20:30:15 -0400

    Setting the FileLogAdapter before Settings classes get instantiated

--- src/UnityExtension/Assets/Editor/GitHub.Unity/EntryPoint.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/EntryPoint.cs
@@ -23,6 +23,12 @@ namespace GitHub.Unity
                 return;
             }
 
+            var logPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData)
+                                .ToNPath().Combine(ApplicationInfo.ApplicationName, "github-unity.log");
+
+            Logging.LoggerFactory = s => new FileLogAdapter(logPath, s);
+            logger = Logging.GetLogger<EntryPoint>();
+
             ServicePointManager.ServerCertificateValidationCallback = ServerCertificateValidationCallback;
             EditorApplication.update += Initialize;
         }
@@ -32,16 +38,10 @@ namespace GitHub.Unity
         {
             EditorApplication.update -= Initialize;
 
-            var logPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData)
-                                .ToNPath().Combine(ApplicationInfo.ApplicationName, "github-unity.log");
-
             if (ApplicationCache.Instance.FirstRun)
             {
-                Logging.Info("Initializing GitHub for Unity version " + ApplicationInfo.Version);
+                Debug.Log("Initializing GitHub for Unity version " + ApplicationInfo.Version);
             }
-
-            Logging.LoggerFactory = s => new FileLogAdapter(logPath, s);
-            logger = Logging.GetLogger<EntryPoint>();
 
             Logging.Info("Initializing GitHub for Unity version " + ApplicationInfo.Version);
 
