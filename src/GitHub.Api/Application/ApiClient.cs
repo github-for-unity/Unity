c48e5281ed0cbfe0922b68fcea887b0bf6a0caea
Don Okuda <don.okuda@gmail.com>
2017-06-20 10:38:15 -0700

    Return Task not void

--- src/GitHub.Api/Application/ApiClient.cs
+++ src/GitHub.Api/Application/ApiClient.cs
@@ -55,13 +55,13 @@ namespace GitHub.Unity
             callback(repo);
         }
 
-        public async void CreateRepository(NewRepository newRepository, Action<Octokit.Repository, Exception> callback, string organization = null)
+        public async Task CreateRepository(NewRepository newRepository, Action<Octokit.Repository, Exception> callback, string organization = null)
         {
             Guard.ArgumentNotNull(callback, "callback");
             await CreateRepositoryInternal(newRepository, callback, organization);
         }
 
-        public async void GetOrganizations(Action<IList<Organization>> callback)
+        public async Task GetOrganizations(Action<IList<Organization>> callback)
         {
             Guard.ArgumentNotNull(callback, "callback");
             var organizations = await GetOrganizationInternal();
