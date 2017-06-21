3b9f3767ab6d1e6a00efa94583feb02ee60c742d
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-06-21 12:55:34 -0400

    Removing some comments

--- src/UnityExtension/Assets/Editor/GitHub.Unity/MetricsInitialization.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/MetricsInitialization.cs
@@ -9,16 +9,12 @@ namespace GitHub.Unity
         [InitializeOnLoadMethod]
         static void Load()
         {
-            Debug.Log("MetricsInitialization.Load");
             EditorApplication.update += Initialize;
         }
 
         private static void Initialize()
         {
             EditorApplication.update -= Initialize;
-
-            Debug.Log("MetricsInitialization.Initialize");
-
             UsageTracker.SetMetricsService(new MetricsService(string.Format("GitHub4Unity{0}", AssemblyVersionInformation.Version)));
         }
     }
