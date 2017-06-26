005119cbb0d421ef89f044590fa73fe24369047c
Andreia Gaita <shana@spoiledcat.net>
2017-06-26 16:40:09 +0200

    Using a singleton for caching data

--- src/UnityExtension/Assets/Editor/GitHub.Unity/ApplicationCache.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/ApplicationCache.cs
@@ -1,63 +1,121 @@
-using System;
-using System.IO;
-using System.Linq;
-using UnityEditorInternal;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GitHub.Unity
 {
-    sealed class ApplicationCache : ScriptableObject
+    sealed class ApplicationCache : ScriptObjectSingleton<ApplicationCache>
     {
-        private static ApplicationCache instance;
-        private static string cachePath;
-
         [SerializeField] private bool firstRun = true;
         public bool FirstRun
         {
-            get { return firstRun; }
-            private set
+            get
             {
-                firstRun = value;
-                Flush();
-            }
-        }
-
-        public static ApplicationCache Instance
-        {
-            get { return instance ?? CreateApplicationCache(EntryPoint.Environment); }
-        }
-
-        private static ApplicationCache CreateApplicationCache(IEnvironment environment)
-        {
-            cachePath = environment.UnityProjectPath + "/Temp/github_cache.yaml";
-
-            if (File.Exists(cachePath))
-            {
-                var objects = InternalEditorUtility.LoadSerializedFileAndForget(cachePath);
-                if (objects.Any())
+                var val = firstRun;
+                if (firstRun)
                 {
-                    instance = objects[0] as ApplicationCache;
-                    if (instance != null)
-                    {
-                        if (instance.FirstRun)
-                        {
-                            instance.FirstRun = false;
-                        }
-
-                        return instance;
-                    }
+                    firstRun = false;
+                    Save(true);
                 }
+                return val;
             }
-
-            instance = CreateInstance<ApplicationCache>();
-            instance.Flush();
-
-            return instance;
         }
 
-        private void Flush()
+        [SerializeField] private string createdDate;
+        public string CreatedDate
         {
-            InternalEditorUtility.SaveToSerializedFileAndForget(new UnityEngine.Object[] { this }, cachePath, true);
+            get { return createdDate; }
+        }
+    }
+
+    [Location("cache/branches.yaml", LocationAttribute.Location.UserFolder)]
+    sealed class BranchCache : ScriptObjectSingleton<BranchCache>, IBranchCache
+    {
+        [SerializeField] private List<GitBranch> localBranches;
+        [SerializeField] private List<GitBranch> remoteBranches;
+        [SerializeField] private List<GitBranch> test;
+        public BranchCache()
+        {
+            test = new List<GitBranch>() { new GitBranch("name", "tracking", false) };
+        }
+
+        public List<GitBranch> LocalBranches
+        {
+            get
+            {
+                if (localBranches == null)
+                    localBranches = new List<GitBranch>();
+                return localBranches;
+            }
+            set
+            {
+                Logging.GetLogger().Debug("Saving branches {0}", value.Join(","));
+                localBranches = value;
+                Save(true);
+            }
+        }
+        public List<GitBranch> RemoteBranches
+        {
+            get
+            {
+                if (remoteBranches == null)
+                    remoteBranches = new List<GitBranch>();
+                return remoteBranches;
+            }
+            set
+            {
+                remoteBranches = value;
+                Save(true);
+            }
+        }
+    }
+
+    [Location("views/branches.yaml", LocationAttribute.Location.UserFolder)]
+    sealed class Favourites : ScriptObjectSingleton<Favourites>
+    {
+        [SerializeField] private List<string> favouriteBranches;
+        public List<string> FavouriteBranches
+        {
+            get
+            {
+                if (favouriteBranches == null)
+                    FavouriteBranches = new List<string>();
+                return favouriteBranches;
+            }
+            set
+            {
+                favouriteBranches = value;
+                Save(true);
+            }
+        }
+
+        public void SetFavourite(string branchName)
+        {
+            if (FavouriteBranches.Contains(branchName))
+                return;
+            FavouriteBranches.Add(branchName);
+            Save(true);
+        }
+
+        public void UnsetFavourite(string branchName)
+        {
+            if (!FavouriteBranches.Contains(branchName))
+                return;
+            FavouriteBranches.Remove(branchName);
+            Save(true);
+        }
+
+        public void ToggleFavourite(string branchName)
+        {
+            if (FavouriteBranches.Contains(branchName))
+                FavouriteBranches.Remove(branchName);
+            else
+                FavouriteBranches.Add(branchName);
+            Save(true);
+        }
+
+        public bool IsFavourite(string branchName)
+        {
+            return FavouriteBranches.Contains(branchName);
         }
     }
 }
