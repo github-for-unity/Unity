6e56c0daabf15463328bdccadf23dfd19f2d2e93
Andreia Gaita <shana@spoiledcat.net>
2017-07-03 15:27:23 +0200

    Bump version to 0.13.0.0

--- common/SolutionInfo.cs
+++ common/SolutionInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 namespace System
 {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.12.0.0";
+        internal const string Version = "0.13.0.0";
     }
 }
