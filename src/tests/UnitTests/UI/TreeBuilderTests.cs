3de86ee5ec33c5b41a5970ae77c4e68b52b04922
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-07-11 11:27:05 -0400

    Adding test

---
+++ src/tests/UnitTests/UI/TreeBuilderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GitHub.Unity;
+using NUnit.Framework;
+
+namespace UnitTests.UI
+{
+
+    [TestFixture]
+    public class TreeBuilderTests
+    {
+        [Test]
+        public void FirstTreeBuilderTest()
+        {
+            var newGitStatusEntries = new List<GitStatusEntry>();
+            var gitStatusEntries = new List<GitStatusEntry>();
+
+            var gitCommitTargets = new List<GitCommitTarget>();
+            var foldedTreeEntries1 = new List<string>();
+
+            Action<FileTreeNode> stateChangeCallback1 = node => { };
+            var fileTreeNode = TreeBuilder.BuildTree4(newGitStatusEntries, gitStatusEntries, gitCommitTargets, foldedTreeEntries1, stateChangeCallback1);
+        }
+    }
+}
