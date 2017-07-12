9b7a25de8e15fae1b7c1efeeff1ec03894d0baae
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-07-12 11:24:11 -0400

    Adding a cmd file to build and package code

--- package.cmd
+++ package.cmd
@@ -1,2 +1,53 @@
-set GITHUB_UNITY_DISABLE=1
-%1\Unity.exe -batchmode -projectPath %~dp0unity\PackageProject -exportPackage Assets\Editor\GitHub github-for-unity-windows.unitypackage -force-free -quit
+@ECHO on
+
+set Configuration=Release
+
+if %1.==. (
+	echo Need path to Unity
+	EXIT /b 1
+)
+
+set UnityPath=%1
+set UnityPath=%UnityPath:"=%
+
+set ChangeConfigurationToDebug=0
+
+if "%2"=="debug" (
+	set ChangeConfigurationToDebug=1
+)
+
+if "%2"=="Debug" (
+	set ChangeConfigurationToDebug=1
+)
+
+if %ChangeConfigurationToDebug%==1 (
+	set Configuration=Debug
+)
+
+set Unity=%UnityPath%\Editor\Unity.exe
+if not exist "%Unity%" ( 
+	echo Cannot find Unity at %Unity%
+	exit /b 1
+) else (
+	cd unity\PackageProject\Assets
+	git clean -xdf
+	cd ..\..\..
+
+	cd src
+	git clean -xdf
+	cd ..
+	
+	common\nuget.exe restore GitHub.Unity.sln
+	xbuild GitHub.Unity.sln /property:Configuration=%Configuration%
+	
+	del /Q unity/PackageProject/Assets/Editor/GitHub/deleteme*
+	del /Q unity/PackageProject/Assets/Editor/GitHub/*.pdb
+	del /Q unity/PackageProject/Assets/Editor/GitHub/*.pdb.meta
+	del /Q unity/PackageProject/Assets/Editor/GitHub/*.xml
+	
+	for /f tokens^=^2^ usebackq^ delims^=^" %%G in (`find "const string Version" common\SolutionInfo.cs`) do (
+		set Version=%%G
+		set GITHUB_UNITY_DISABLE=1
+		"%Unity%" -batchmode -projectPath "%~dp0unity\PackageProject" -exportPackage Assets/Editor/GitHub github-for-unity-%Version%-alpha.unitypackage -force-free -quit
+	)
+)
