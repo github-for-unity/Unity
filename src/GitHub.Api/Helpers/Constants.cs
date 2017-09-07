b07f95a94740fb72bc59f2187b29e0b578430054
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-09-07 09:59:23 -0400

    Setting software versions

--- src/GitHub.Api/Helpers/Constants.cs
+++ src/GitHub.Api/Helpers/Constants.cs
@@ -8,7 +8,7 @@ namespace GitHub.Unity
         public const string GitInstallPathKey = "GitInstallPath";
         public const string TraceLoggingKey = "EnableTraceLogging";
 
-        public static readonly SoftwareVersion MinimumGitVersion = new SoftwareVersion(2, 11, 1);
-        public static readonly SoftwareVersion MinimumGitLfsVersion = new SoftwareVersion(2, 0, 2);
+        public static readonly SoftwareVersion MinimumGitVersion = new SoftwareVersion(2, 11, 0);
+        public static readonly SoftwareVersion MinimumGitLfsVersion = new SoftwareVersion(2, 2, 0);
     }
 }
