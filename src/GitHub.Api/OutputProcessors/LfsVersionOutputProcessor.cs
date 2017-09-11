6d94a625a8c23ab3499a1e97f90660292d6f5712
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-09-11 16:53:40 -0400

    Changing string parsing to regexes

--- src/GitHub.Api/OutputProcessors/LfsVersionOutputProcessor.cs
+++ src/GitHub.Api/OutputProcessors/LfsVersionOutputProcessor.cs
@@ -1,21 +1,26 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace GitHub.Unity
 {
     class LfsVersionOutputProcessor : BaseOutputProcessor<Version>
     {
+        public static Regex GitLfsVersionRegex = new Regex(@"git-lfs/([\d]+)\.([\d]+)\.([\d]+)");
+
         public override void LineReceived(string line)
         {
             if (String.IsNullOrEmpty(line))
                 return;
 
-            var gitVersion = "git-lfs/";
-            if (line.StartsWith(gitVersion))
-            {
-                line = line.Substring(gitVersion.Length, line.IndexOf(" ", StringComparison.InvariantCultureIgnoreCase) - gitVersion.Length);
-                var strings = line.Split(new[] { "." }, StringSplitOptions.None);
+            var match = GitLfsVersionRegex.Match(line);
 
-                RaiseOnEntry(new Version(Int32.Parse(strings[0]), Int32.Parse(strings[1]), Int32.Parse(strings[2])));
+            if (match.Groups.Count > 0)
+            {
+                var major = Int32.Parse(match.Groups[1].Value);
+                var minor = Int32.Parse(match.Groups[2].Value);
+                var build = Int32.Parse(match.Groups[3].Value);
+                var version = new Version(major, minor, build);
+                RaiseOnEntry(version);
             }
         }
     }
--- src/GitHub.Api/OutputProcessors/VersionOutputProcessor.cs
+++ src/GitHub.Api/OutputProcessors/VersionOutputProcessor.cs
@@ -1,22 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace GitHub.Unity
 {
     class VersionOutputProcessor : BaseOutputProcessor<Version>
     {
+        public static Regex GitVersionRegex = new Regex(@"git version ([\d]+)\.([\d]+)\.([\d]+)");
+
         public override void LineReceived(string line)
         {
             if (String.IsNullOrEmpty(line))
                 return;
 
-            var gitVersion = "git version ";
-            if (line.StartsWith(gitVersion))
-            {
-                line = line.Substring(gitVersion.Length);
-                var strings = line.Split(new[] { "." }, StringSplitOptions.None);
+            var match = GitVersionRegex.Match(line);
 
-                RaiseOnEntry(new Version(Int32.Parse(strings[0]), Int32.Parse(strings[1]), Int32.Parse(strings[2])));
+            if (match.Groups.Count > 0)
+            {
+                var major = Int32.Parse(match.Groups[1].Value);
+                var minor = Int32.Parse(match.Groups[2].Value);
+                var build = Int32.Parse(match.Groups[3].Value);
+                var version = new Version(major, minor, build);
+                RaiseOnEntry(version);
             }
         }
     }
