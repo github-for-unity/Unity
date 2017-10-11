490f92bbf0bfc7742991d70dfd9330a8c76f9aa9
Don Okuda <don.okuda@gmail.com>
2017-10-11 15:45:18 -0700

    Remove header in Initialize Repository view

--- src/UnityExtension/Assets/Editor/GitHub.Unity/UI/InitProjectView.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/UI/InitProjectView.cs
@@ -12,7 +12,6 @@ namespace GitHub.Unity
     [Serializable]
     class InitProjectView : Subview
     {
-        private const string NoRepoTitle = "No Git repository found for this project";
         private const string NoRepoDescription = "Initialize a Git repository to track changes and collaborate with others.";
 
         [SerializeField] private bool isBusy;
@@ -32,35 +31,6 @@ namespace GitHub.Unity
 
         public override void OnGUI()
         {
-            var headerRect = EditorGUILayout.BeginHorizontal(Styles.HeaderBoxStyle);
-            {
-                GUILayout.Space(5);
-                GUILayout.BeginVertical(GUILayout.Width(16));
-                {
-                    GUILayout.Space(5);
-
-                    var iconRect = GUILayoutUtility.GetRect(new GUIContent(Styles.BigLogo), GUIStyle.none, GUILayout.Height(20), GUILayout.Width(20));
-                    iconRect.y = headerRect.center.y - (iconRect.height / 2);
-                    GUI.DrawTexture(iconRect, Styles.BigLogo, ScaleMode.ScaleToFit);
-
-                    GUILayout.Space(5);
-                }
-                GUILayout.EndVertical();
-
-                GUILayout.Space(5);
-
-                GUILayout.BeginVertical();
-                {
-                    var headerContent = new GUIContent(NoRepoTitle);
-                    var headerTitleRect = GUILayoutUtility.GetRect(headerContent, Styles.HeaderTitleStyle);
-                    headerTitleRect.y = headerRect.center.y - (headerTitleRect.height / 2);
-
-                    GUI.Label(headerTitleRect, headerContent, Styles.HeaderTitleStyle);
-                }
-                GUILayout.EndVertical();
-            }
-            EditorGUILayout.EndHorizontal();
-
             GUILayout.BeginVertical(Styles.GenericBoxStyle);
             {
                 GUILayout.FlexibleSpace();
