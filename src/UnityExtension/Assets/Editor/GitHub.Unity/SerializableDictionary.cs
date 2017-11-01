034ba9cc3137bc9a599794930edc2ea6cb82e05c
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-10-31 20:06:45 -0400

    Fixing exception message

--- src/UnityExtension/Assets/Editor/GitHub.Unity/SerializableDictionary.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/SerializableDictionary.cs
@@ -34,7 +34,7 @@ namespace GitHub.Unity
             this.Clear();
 
             if (keys.Count != values.Count)
-                throw new System.Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable."));
+                throw new Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable.", keys.Count, values.Count));
 
             for (int i = 0; i < keys.Count; i++)
                 this.Add(keys[i], values[i]);
