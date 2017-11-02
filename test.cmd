d4cea976446b7cf06fbbf36a7db8fee256ced600
Meaghan Lewis <meaghanlewis@github.com>
2017-11-02 10:59:38 -0700

    Add initial test.cmd script

---
+++ test.cmd
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+set Configuration=Release
+
+:: make sure at Unity project root directory
+set NunitDirectory=packages\NUnit.Runners.2.6.4\tools
+echo %NunitDirectory%
+set ConsoleRunner=%NunitDirectory%\nunit-console.exe
+echo %ConsoleRunner%
+
+:: run tests
+echo Running "build\IntegrationTests\IntegrationTests.dll" "build\IntegrationTests\TestUtils.dll" "build\TaskSystemIntegrationTests\TaskSystemIntegrationTests.dll" "build\UnitTests\TestUtils.dll" "build\UnitTests\UnitTests.dll" "src\tests\TestUtils\bin\Release\TestUtils.dll" %1
+call %ConsoleRunner% "build\IntegrationTests\IntegrationTests.dll" "build\IntegrationTests\TestUtils.dll" "build\TaskSystemIntegrationTests\TaskSystemIntegrationTests.dll" "build\UnitTests\TestUtils.dll" "build\UnitTests\UnitTests.dll" "src\tests\TestUtils\bin\Release\TestUtils.dll" %1
+
+endlocal
