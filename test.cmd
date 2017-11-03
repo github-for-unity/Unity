0cf04a19bcf982f5d7451edcd0d225313f327bcb
Meaghan Lewis <meaghanlewis@github.com>
2017-11-03 16:06:03 -0700

    Update parameters for test.cmd
    Config parameter which is Debug by default.
    Exclude parameter which is an empty string by default.

--- test.cmd
+++ test.cmd
@@ -1,6 +1,16 @@
 @echo off
 setlocal
 
+set Config=Debug
+if not %1.==. (
+  set Config=%1
+)
+
+set Exclude=''
+if not %2.==. (
+  set Exclude=%2
+)
+
 :: make sure at Unity project root directory
 set NunitDirectory=packages\NUnit.Runners.2.6.4\tools
 echo %NunitDirectory%
@@ -8,7 +18,7 @@ set ConsoleRunner=%NunitDirectory%\nunit-console.exe
 echo %ConsoleRunner%
 
 :: run tests
-echo Running "build\IntegrationTests\IntegrationTests.dll" "build\IntegrationTests\TestUtils.dll" "build\TaskSystemIntegrationTests\TaskSystemIntegrationTests.dll" "build\UnitTests\TestUtils.dll" "build\UnitTests\UnitTests.dll" "src\tests\TestUtils\bin\Release\TestUtils.dll" %1
-call %ConsoleRunner% "build\IntegrationTests\IntegrationTests.dll" "build\IntegrationTests\TestUtils.dll" "build\TaskSystemIntegrationTests\TaskSystemIntegrationTests.dll" "build\UnitTests\TestUtils.dll" "build\UnitTests\UnitTests.dll" "src\tests\TestUtils\bin\Release\TestUtils.dll" %1
+echo Running "build\IntegrationTests\IntegrationTests.dll" "build\IntegrationTests\TestUtils.dll" "build\TaskSystemIntegrationTests\TaskSystemIntegrationTests.dll" "build\UnitTests\TestUtils.dll" "build\UnitTests\UnitTests.dll" "src\tests\TestUtils\bin\Release\TestUtils.dll" /config=%Config% /exclude=%Exclude%
+call %ConsoleRunner% "build\IntegrationTests\IntegrationTests.dll" "build\IntegrationTests\TestUtils.dll" "build\TaskSystemIntegrationTests\TaskSystemIntegrationTests.dll" "build\UnitTests\TestUtils.dll" "build\UnitTests\UnitTests.dll" "src\tests\TestUtils\bin\Release\TestUtils.dll" /config=%Config% /exclude=%Exclude%
 
 endlocal
