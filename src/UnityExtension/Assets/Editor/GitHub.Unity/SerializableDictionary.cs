08705a7250605a79fbc9a6fd1aa35df7144e2a13
Andreia Gaita <shana@spoiledcat.net>
2017-11-06 20:28:17 -0800

    Format SerializableDictionary according to our coding style

--- src/UnityExtension/Assets/Editor/GitHub.Unity/SerializableDictionary.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/SerializableDictionary.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-
 
 namespace GitHub.Unity
 {
@@ -11,18 +9,15 @@ namespace GitHub.Unity
     [Serializable]
     public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
     {
-        [SerializeField]
-        private List<TKey> keys = new List<TKey>();
-
-        [SerializeField]
-        private List<TValue> values = new List<TValue>();
+        [SerializeField] private List<TKey> keys = new List<TKey>();
+        [SerializeField] private List<TValue> values = new List<TValue>();
 
         // save the dictionary to lists
         public void OnBeforeSerialize()
         {
             keys.Clear();
             values.Clear();
-            foreach (KeyValuePair<TKey, TValue> pair in this)
+            foreach (var pair in this)
             {
                 keys.Add(pair.Key);
                 values.Add(pair.Value);
@@ -32,13 +27,19 @@ namespace GitHub.Unity
         // load dictionary from lists
         public void OnAfterDeserialize()
         {
-            this.Clear();
+            Clear();
 
             if (keys.Count != values.Count)
-                throw new Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable.", keys.Count, values.Count));
+            {
+                throw new Exception(
+                    string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable.",
+                        keys.Count, values.Count));
+            }
 
-            for (int i = 0; i < keys.Count; i++)
-                this.Add(keys[i], values[i]);
+            for (var i = 0; i < keys.Count; i++)
+            {
+                Add(keys[i], values[i]);
+            }
         }
     }
 }
