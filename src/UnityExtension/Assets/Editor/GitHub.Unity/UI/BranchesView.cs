484d2ef135d47bb4aedf332590a1ba60c5d1bdf0
Andreia Gaita <shana@spoiledcat.net>
2017-11-15 20:06:47 +0100

    Don't need this anymore, Load always happens before Render

--- src/UnityExtension/Assets/Editor/GitHub.Unity/UI/BranchesView.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/UI/BranchesView.cs
@@ -551,9 +551,6 @@ namespace GitHub.Unity
 
             public Rect Render(Rect rect, Action<TreeNode> singleClick = null, Action<TreeNode> doubleClick = null)
             {
-                if (!nodes.Any())
-                    return rect;
-
                 RequiresRepaint = false;
                 rect = new Rect(0f, rect.y, rect.width, ItemHeight);
 
