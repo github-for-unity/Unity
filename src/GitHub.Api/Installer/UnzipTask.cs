3831d92bfbd9c88b043a5b879916e39159a02bf8
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-12-27 17:57:14 -0500

    Unizip Task & Test

---
+++ src/GitHub.Api/Installer/UnzipTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GitHub.Unity
+{
+    class UnzipTask: TaskBase
+    {
+        protected static ILogging Logger { get; } = Logging.GetLogger<UnzipTask>();
+
+        private string archiveFilePath;
+        private string extractedPath;
+        private IProgress<float> zipFileProgress;
+        private IProgress<long> estimatedDurationProgress;
+
+        public UnzipTask(CancellationToken token, string archiveFilePath, string extractedPath, IProgress<float> zipFileProgress = null, IProgress<long> estimatedDurationProgress = null)
+            : base(token)
+        {
+            this.archiveFilePath = archiveFilePath;
+            this.extractedPath = extractedPath;
+            this.zipFileProgress = zipFileProgress;
+            this.estimatedDurationProgress = estimatedDurationProgress;
+        }
+
+        protected override void Run(bool success)
+        {
+            base.Run(success);
+
+            UnzipArchive();
+        }
+
+        private void UnzipArchive()
+        {
+            Logger.Trace("Zip File: {0}", archiveFilePath);
+            Logger.Trace("Target Path: {0}", extractedPath);
+
+            ZipHelper.ExtractZipFile(archiveFilePath, extractedPath, Token, zipFileProgress, estimatedDurationProgress);
+
+            Logger.Trace("Completed");
+        }
+    }
+}
