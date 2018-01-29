f6826b5f2e37f1fa0e6da021c6a6a85200aeebae
Andreia Gaita <shana@spoiledcat.net>
2018-01-29 21:32:35 +0100

    Add test for new UriString.Filename property

---
+++ src/tests/UnitTests/Primitives/UriStringTests.cs
@@ -0,0 +1,21 @@
+﻿using GitHub.Unity;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests.Primitives
+{
+    [TestFixture]
+    class UriStringTests
+    {
+        [TestCase("http://url.com/path/file.zip?cb=1", "file.zip")]
+        [TestCase("http://url.com/path/file?cb=1", "file")]
+        public void FilenameParsing(string url, string expectedFilename)
+        {
+            var uriString = new UriString(url);
+            Assert.AreEqual(expectedFilename, uriString.Filename);
+        }
+    }
+}
