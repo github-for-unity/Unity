f3516a898a4e3406a44ba7451e6acec0337cfacd
Andreia Gaita <shana@spoiledcat.net>
2018-02-23 16:40:15 +0100

    Bump version to 0.26.2

--- common/SolutionInfo.cs
+++ common/SolutionInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 namespace System
 {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.27.0";
+        internal const string Version = "0.26.2";
     }
 }
