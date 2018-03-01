caf09610db5071631f06602c486a37d79909849c
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-03-01 10:49:17 -0500

    I understand how i'm supposed to do this now

--- octorun-modern/index.js
+++ octorun-modern/index.js
@@ -1,26 +1,68 @@
-require("dotenv").config();
+const readlineSync = require("readline-sync");
+const octokit = require('@octokit/rest')({
+    timeout: 0, // 0 means no request timeout
+    requestMedia: 'application/vnd.github.v3+json',
+    headers: {
+      'user-agent': 'octokit/rest.js v1.2.3' // v1.2.3 will be current version
+    },
+  
+    // change for custom GitHub Enterprise URL
+    //host: 'api.github.com',
+    //pathPrefix: '',
+    //protocol: 'https',
+    //port: 443,
+  
+    // Node only: advanced request options can be passed as http(s) agent
+    //agent: undefined
+  });
 
 console.log("NodeJS Path: ", process.argv[0]);
 
-console.log(process.env.OCTOKIT_CLIENT_ID);
-console.log(process.env.OCTOKIT_CLIENT_SECRET);
+require("dotenv").config();
 
-var GitHub = require('@octokit/rest');
-var gitHub = new GitHub();
+const clientId = process.env.OCTOKIT_CLIENT_ID;
+const clientSecret = process.env.OCTOKIT_CLIENT_SECRET;
 
-var authParams = {
-    client_id: process.env.OCTOKIT_CLIENT_ID,
-    client_secret: process.env.OCTOKIT_CLIENT_SECRET,
-    scopes: ["user", "repo", "gist", "write:public_key"]
-};
+const appName = process.env.OCTORUN_APP_NAME | "octorun";
+let user = process.env.OCTORUN_USER;
+const token = process.env.OCTORUN_TOKEN;
 
-gitHub.authorization.getOrCreateAuthorizationForApp(authParams, function (error, result) {
-    if (error) {
-        console.log("error", error, error.stack);
-    }
-    else {
-        console.log("result", result);
-    }
+const scopes = ["user", "repo", "gist", "write:public_key"];
 
-    process.exit();
-});
+if(user != null && token != null)
+{
+
+}
+else
+{
+    user = readlineSync.question('User: ');
+    
+    var pwd = readlineSync.question('Password: ', {
+        hideEchoBack: true
+    });
+
+    octokit.authenticate({
+        type:"basic",
+        username:user,
+        password:pwd
+    });
+
+    octokit.authorization.create({
+        scopes: scopes,
+        note: appName,
+        client_id: clientId,
+        client_secret: clientSecret
+    }, function(err, res) {
+
+        console.log("err", err, "res", res);
+
+        if(err)
+        {
+            
+        }
+        else
+        {
+
+        }
+    });
+}
