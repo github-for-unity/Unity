85d0a919e9d46e6c8ce6324dc78f0b69321d68fd
Stanley Goldman <StanleyGoldman@github.com>
2018-04-20 10:32:37 -0400

    Writing the commit message to a file before processing (#703)
    * Writing the commit message to a file before processing
    
    * Reducing repetition
    
    * Using string interpolation for clarity
    
    * Using nameof for sanity
    
    * Using NPath for sanity
    
    * Cleaning up after ourselves
    
    * Writing the message to a file in the GitCommitTask
    
    * Tasks shouldn't do anything after OnEnd is raised

--- src/GitHub.Api/Git/Tasks/GitCommitTask.cs
+++ src/GitHub.Api/Git/Tasks/GitCommitTask.cs
@@ -6,7 +6,12 @@ namespace GitHub.Unity
     class GitCommitTask : ProcessTask<string>
     {
         private const string TaskName = "git commit";
+
+        private readonly string message;
+        private readonly string body;
         private readonly string arguments;
+
+        private NPath tempFile;
 
         public GitCommitTask(string message, string body,
             CancellationToken token, IOutputProcessor<string> processor = null)
@@ -14,11 +19,24 @@ namespace GitHub.Unity
         {
             Guard.ArgumentNotNullOrWhiteSpace(message, "message");
 
+            this.message = message;
+            this.body = body ?? string.Empty;
+
             Name = TaskName;
-            arguments = "-c i18n.commitencoding=utf8 commit ";
-            arguments += String.Format(" -m \"{0}\"", message);
-            if (!String.IsNullOrEmpty(body))
-                arguments += String.Format(" -m \"{0}\"", body);
+            tempFile = NPath.GetTempFilename("GitCommitTask");
+            arguments = $"-c i18n.commitencoding=utf8 commit --file \"{tempFile}\"";
+        }
+
+        protected override void RaiseOnStart()
+        {
+            base.RaiseOnStart();
+            tempFile.WriteAllLines(new [] { message, Environment.NewLine, body });
+        }
+
+        protected override void RaiseOnEnd()
+        {
+            tempFile.DeleteIfExists();
+            base.RaiseOnEnd();
         }
 
         public override string ProcessArguments { get { return arguments; } }
