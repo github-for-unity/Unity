56b2ce0d2375f1bb69cb79262830a2e38c2d8338
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-05-15 12:04:09 -0400

    Updating ApplicationConfiguration webTimeout from settings

--- src/GitHub.Api/Application/ApplicationManagerBase.cs
+++ src/GitHub.Api/Application/ApplicationManagerBase.cs
@@ -42,6 +42,13 @@ namespace GitHub.Unity
             Platform = new Platform(Environment);
 
             LogHelper.TracingEnabled = UserSettings.Get(Constants.TraceLoggingKey, false);
+
+            int webTimeout;
+            if (int.TryParse(UserSettings.Get(Constants.WebTimeoutKey), out webTimeout))
+            {
+                ApplicationConfiguration.WebTimeout = webTimeout;
+            }
+
             ProcessManager = new ProcessManager(Environment, Platform.GitEnvironment, CancellationToken);
             Platform.Initialize(ProcessManager, TaskManager);
             GitClient = new GitClient(Environment, ProcessManager, TaskManager.Token);
