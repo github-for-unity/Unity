e69d1488f5f53fed4bac13dbcd4f2f24126aae39
Stanley Goldman <StanleyGoldman@github.com>
2018-05-29 16:29:37 -0400

    Returning the correct exception

--- src/GitHub.Api/Tasks/ActionTask.cs
+++ src/GitHub.Api/Tasks/ActionTask.cs
@@ -46,7 +46,7 @@ namespace GitHub.Unity
                 }
                 else
                 {
-                    aggregateTask.TrySetException(ex);
+                    aggregateTask.TrySetException(exception);
                 }
             }
         }
