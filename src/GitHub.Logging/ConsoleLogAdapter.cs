ff3fa10fe4c42f3d8d59b0307f49a93fc15bb1e0
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-05-31 16:18:04 -0400

    Fixing up the ConsoleLogAdapter

--- src/GitHub.Logging/ConsoleLogAdapter.cs
+++ src/GitHub.Logging/ConsoleLogAdapter.cs
@@ -5,41 +5,40 @@ namespace GitHub.Logging
 {
     public class ConsoleLogAdapter : LogAdapterBase
     {
-        private string GetMessage(string level, string context, string message)
+        private string GetMessage(string context, string message)
         {
             var time = DateTime.Now.ToString("HH:mm:ss.fff tt");
-            var threadId = Thread.CurrentThread.ManagedThreadId;
-            return string.Format("{0} {1} [{2,2}] {3} {4}", time, level, threadId, context, message);
+            return $"{time} {context} {message}";
         }
 
         public override void Info(string context, string message)
         {
-            Console.WriteLine(message);
+            WriteLine(context, message);
         }
 
         public override void Debug(string context, string message)
         {
-            WriteLine("DEBUG", context, message);
+            WriteLine(context, message);
         }
 
         public override void Trace(string context, string message)
         {
-            WriteLine("TRACE", context, message);
+            WriteLine(context, message);
         }
 
         public override void Warning(string context, string message)
         {
-            WriteLine("WARN", context, message);
+            WriteLine(context, message);
         }
 
         public override void Error(string context, string message)
         {
-            Console.Error.WriteLine(message);
+            WriteLine(context, message);
         }
 
-        private void WriteLine(string level, string context, string message)
+        private void WriteLine(string context, string message)
         {
-            Console.WriteLine(GetMessage(level, context, message));
+            Console.WriteLine(GetMessage(context, message));
         }
     }
 }
