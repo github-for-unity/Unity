36417820643149f28801fcf6d850805d30485764
Marcelo Oliveira <capyvara@gmail.com>
2018-06-16 03:24:12 -0300

    Compiles with both 3.5 and 4.5+ frameworks

---
+++ src/GitHub.Api/Tasks/TaskEx.cs
@@ -0,0 +1,57 @@
+﻿#if !NET35
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GitHub.Unity
+{
+    public static class TaskEx
+    {
+        public static Task<TResult> FromResult<TResult>(TResult result)
+        {
+            return Task.FromResult(result);
+        }
+
+        public static Task<Task> WhenAny(System.Collections.Generic.IEnumerable<Task> tasks)
+        {
+            return Task.WhenAny(tasks);
+        }
+
+        public static Task<Task> WhenAny(params Task[] tasks)
+        {
+            return Task.WhenAny(tasks);
+        }
+
+        public static Task<Task<TResult>> WhenAny<TResult>(IEnumerable<Task<TResult>> tasks)
+        {
+            return Task.WhenAny(tasks);
+        }
+
+        public static Task<Task<TResult>> WhenAny<TResult>(params Task<TResult>[] tasks)        
+        {
+            return Task.WhenAny(tasks);
+        }
+
+        public static Task Delay(int millisecondsDelay)
+        {
+            return Task.Delay(millisecondsDelay);
+        }
+
+        public static Task Delay(int millisecondsDelay, CancellationToken cancellationToken)
+        {
+            return Task.Delay(millisecondsDelay, cancellationToken);
+        }
+
+        public static Task Delay(TimeSpan delay)
+        {
+            return Task.Delay(delay);
+        }
+
+        public static Task Delay(TimeSpan delay, CancellationToken cancellationToken)
+        {
+            return Task.Delay(delay, cancellationToken);
+        }
+    }
+}
+#endif
