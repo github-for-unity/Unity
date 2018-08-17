b67daee7c5f7211a3b52cb4723f29f8d5438a7e7
Andreia Gaita <shana@spoiledcat.net>
2018-08-17 15:52:11 +0200

    Add support for 2018 4.6 profile

--- src/GitHub.Api/Properties/AssemblyInfo.cs
+++ src/GitHub.Api/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GitHub.Api")]
-[assembly: AssemblyDescription("GitHub Api")]
+[assembly: AssemblyDescription("GitHub for Unity API")]
 [assembly: Guid("4B424108-D0E8-4BF9-9B0C-4FB49E532AB9")]
 [assembly: InternalsVisibleTo("GitHub.Unity")]
+[assembly: InternalsVisibleTo("GitHub.Unity.45")]
