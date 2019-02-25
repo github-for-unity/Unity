3eb1b5e623cf94dc5dd527d5b5211e8d68bc6930
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-02-25 10:52:51 -0500

    Calling AssetDatabase.Refresh after discard

--- src/UnityExtension/Assets/Editor/GitHub.Unity/UI/ChangesView.cs
+++ src/UnityExtension/Assets/Editor/GitHub.Unity/UI/ChangesView.cs
@@ -244,6 +244,9 @@ namespace GitHub.Unity
                 }
 
                 Repository.DiscardChanges(discardEntries)
+                          .ThenInUI(() => {
+                              AssetDatabase.Refresh();
+                          })
                           .Start();
             });
 
