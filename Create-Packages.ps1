e92ba6d13d0d45e02d99d6408329dd50befcbb19
Andreia Gaita <shana@spoiledcat.net>
2019-08-07 14:02:25 +0200

    Remove stale meta files

--- Create-Packages.ps1
+++ Create-Packages.ps1
@@ -27,6 +27,6 @@ $apiVersion = $apiVersionData.AssemblyInformationalVersion
 Run-Command -Fatal { & "$rootDirectory\submodules\packaging\unitypackage\run.ps1" -PathToPackage "$rootDirectory\unity\GHfU-net35" -OutputFolder "$rootDirectory" -PackageName "github-for-unity-net20-$uiVersion" }
 Run-Command -Fatal { & "$rootDirectory\submodules\packaging\unitypackage\run.ps1" -PathToPackage "$rootDirectory\unity\GHfU-net471" -OutputFolder "$rootDirectory" -PackageName "github-for-unity-$uiVersion" }
 
-Run-Command -Fatal { & "$rootDirectory\src\git-for-unity\create-unity-packages\run.ps1" -PathToPackage "$rootDirectory\packages\com.github.ui" -OutputFolder "$rootDirectory" -PackageName com.github.ui -Version "$uiVersion" -Ignores "$rootDirectory\packages\com.github.ui\.npmignore" }
-Run-Command -Fatal { & "$rootDirectory\src\git-for-unity\create-unity-packages\run.ps1" -PathToPackage "$rootDirectory\packages\com.unity.git.api" -OutputFolder "$rootDirectory" -PackageName com.unity.git.api -Version "$apiVersion" -Ignores "$rootDirectory\packages\com.unity.git.api\.npmignore" }
+Run-Command -Fatal { & "$rootDirectory\src\git-for-unity\packaging\create-unity-packages\run.ps1" -PathToPackage "$rootDirectory\packages\com.github.ui" -OutputFolder "$rootDirectory" -PackageName com.github.ui -Version "$uiVersion" -Ignores "$rootDirectory\packages\com.github.ui\.npmignore" }
+Run-Command -Fatal { & "$rootDirectory\src\git-for-unity\packaging\create-unity-packages\run.ps1" -PathToPackage "$rootDirectory\packages\com.unity.git.api" -OutputFolder "$rootDirectory" -PackageName com.unity.git.api -Version "$apiVersion" -Ignores "$rootDirectory\packages\com.unity.git.api\.npmignore" }
 
--- create-packages.sh
+++ create-packages.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -eu
+#!/bin/sh -eux
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 commitcount=$(git rev-list  --count HEAD)
@@ -14,8 +14,8 @@ commit=$(cd src/git-for-unity && git log -n1 --pretty=format:%h)
 version=$(sed -En 's,.*AssemblyInformationalVersion = "(.*)".*,\1,p' build/obj/Release/net471/Unity.VersionControl.Git.Version.cs)
 versiongitapi="${version}.${commitcount}-${commit}"
 
-$DIR/submodules/packaging/unitypackage/run.sh --path "$DIR/unity/GHfU-net35" --out "$DIR" --file github-for-unity-net20-$version35 --project
-$DIR/submodules/packaging/unitypackage/run.sh --path "$DIR/unity/GHfU-net471" --out "$DIR" --file github-for-unity-$version471 --project
+#$DIR/submodules/packaging/unitypackage/run.sh --path "$DIR/unity/GHfU-net35" --out "$DIR" --file github-for-unity-net20-$version35
+#$DIR/submodules/packaging/unitypackage/run.sh --path "$DIR/unity/GHfU-net471" --out "$DIR" --file github-for-unity-$version471
 
-$DIR/src/git-for-unity/create-unity-packages/run.sh --path "$DIR/packages/com.github.ui" --out "$DIR" --name com.github.ui --version "$version471" --ignores "$DIR/packages/com.github.ui/.npmignore"
-$DIR/src/git-for-unity/create-unity-packages/run.sh --path "$DIR/packages/com.unity.git.api" --out "$DIR" --name com.unity.git.api --version "$versiongitapi" --ignores "$DIR/packages/com.unity.git.api/.npmignore"
+$DIR/src/git-for-unity/packaging/create-unity-packages/run.sh --path "$DIR/build/packages/com.github.ui" --out "$DIR" --name com.github.ui --version \"$version471\" --ignores "$DIR/build/packages/com.github.ui/.npmignore"
+$DIR/src/git-for-unity/packaging/create-unity-packages/run.sh --path "$DIR/build/packages/com.unity.git.api" --out "$DIR" --name com.unity.git.api --version "$versiongitapi" --ignores "$DIR/build/packages/com.unity.git.api/.npmignore"
