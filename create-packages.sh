c5604e754cf72bde0d38708a4f10e9b16bd23012
Andreia Gaita <shana@spoiledcat.net>
2019-08-07 14:16:47 +0200

    Stamp packages with global version, put build results in an artifacts folder

--- create-packages.sh
+++ create-packages.sh
@@ -1,21 +1,14 @@
 #!/bin/sh -eux
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-commitcount=$(git rev-list  --count HEAD)
-commit=$(git log -n1 --pretty=format:%h)
+outdir="$DIR/artifacts"
 
-version=$(sed -En 's,.*AssemblyInformationalVersion = "(.*)".*,\1,p' build/obj/Release/net35/GitHub.Unity.Version.cs)
-version35="${version}.${commitcount}-${commit}"
-version=$(sed -En 's,.*AssemblyInformationalVersion = "(.*)".*,\1,p' build/obj/Release/net471/GitHub.Unity.Version.cs)
-version471="${version}.${commitcount}-${commit}"
+version35=$(sed -En 's,.*AssemblyInformationalVersion = "(.*)".*,\1,p' build/obj/Release/net35/GitHub.Unity.Version.cs)
+version471=$(sed -En 's,.*AssemblyInformationalVersion = "(.*)".*,\1,p' build/obj/Release/net471/GitHub.Unity.Version.cs)
+versiongitapi=$(sed -En 's,.*AssemblyInformationalVersion = "(.*)".*,\1,p' build/obj/Release/net471/Unity.VersionControl.Git.Version.cs)
 
-commitcount=$(cd src/git-for-unity && git rev-list  --count HEAD)
-commit=$(cd src/git-for-unity && git log -n1 --pretty=format:%h)
-version=$(sed -En 's,.*AssemblyInformationalVersion = "(.*)".*,\1,p' build/obj/Release/net471/Unity.VersionControl.Git.Version.cs)
-versiongitapi="${version}.${commitcount}-${commit}"
+#$DIR/submodules/packaging/unitypackage/run.sh --path "$DIR/unity/GHfU-net35" --out "$outdir" --file github-for-unity-net20-$version35
+#$DIR/submodules/packaging/unitypackage/run.sh --path "$DIR/unity/GHfU-net471" --out "$outdir" --file github-for-unity-$version471
 
-#$DIR/submodules/packaging/unitypackage/run.sh --path "$DIR/unity/GHfU-net35" --out "$DIR" --file github-for-unity-net20-$version35
-#$DIR/submodules/packaging/unitypackage/run.sh --path "$DIR/unity/GHfU-net471" --out "$DIR" --file github-for-unity-$version471
-
-$DIR/src/git-for-unity/packaging/create-unity-packages/run.sh --path "$DIR/build/packages/com.github.ui" --out "$DIR" --name com.github.ui --version \"$version471\" --ignores "$DIR/build/packages/com.github.ui/.npmignore"
-$DIR/src/git-for-unity/packaging/create-unity-packages/run.sh --path "$DIR/build/packages/com.unity.git.api" --out "$DIR" --name com.unity.git.api --version "$versiongitapi" --ignores "$DIR/build/packages/com.unity.git.api/.npmignore"
+$DIR/src/git-for-unity/packaging/create-unity-packages/run.sh --path "$DIR/build/packages/com.github.ui" --out "$outdir" --name com.github.ui --version $version471 --ignores "$DIR/build/packages/com.github.ui/.npmignore"
+$DIR/src/git-for-unity/packaging/create-unity-packages/run.sh --path "$DIR/build/packages/com.unity.git.api" --out "$outdir" --name com.unity.git.api --version "$versiongitapi" --ignores "$DIR/build/packages/com.unity.git.api/.npmignore"
