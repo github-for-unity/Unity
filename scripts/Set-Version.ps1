9b44bcd155ea9d51bfabeba445b537dbb649bcf0
Andreia Gaita <shana@spoiledcat.net>
2019-08-12 12:01:37 +0200

    Versioning standardization and scripts

--- scripts/Set-Version.ps1
+++ scripts/Set-Version.ps1
@@ -1,7 +1,19 @@
 Param(
-	[Parameter(Mandatory=$true)]
-	[int]
-	$PatchVersion,
+	[string]
+	$NewVersion = ''
+	,
+	[switch]
+	$BumpMajor = $false
+	,
+	[switch]
+	$BumpMinor = $false
+	,
+	[switch]
+	$BumpPatch = $false
+	,
+	[switch]
+	$BumpBuild = $false
+	,
 	[switch]
 	$Trace = $false
 )
@@ -12,31 +24,90 @@ if ($Trace) { Set-PSDebug -Trace 1 }
 . $PSScriptRoot\helpers.ps1 | out-null
 
 & {
-    Trap {
-        Write-Output "Setting version failed"
-        Write-Output "Error: $_"
-        exit 0
-    }
+	Trap {
+		Write-Output "Setting version failed"
+		Write-Output "Error: $_"
+		exit 0
+	}
+
+	if ($NewVersion -eq '') {
+		if (!$BumpMajor -and !$BumpMinor -and !$BumpPatch -and !$BumpBuild){
+			Die -1 "You need to indicate which part of the version to update via -BumpMajor/-BumpMinor/-BumpPatch/-BumpBuild flags or a custom version via -NewVersion"
+		}
+	}
 
 	$source = Get-Content -Raw $PSScriptRoot\TheVersion.cs
 	Add-Type -TypeDefinition "$source"
 
-	function PatchVersionFile([string]$versionFile, [int]$newPatchValue) {
+	function Read-Version([string]$versionFile) {
 		$versionjson = Get-Content $versionFile | ConvertFrom-Json
 		$version = $versionjson.version
 		$parsed = [TheVersion]::Parse("$version")
-		$newVersion = $parsed.SetPatch($newPatchValue)
+		$parsed
+	}
+
+	function Write-Version([string]$versionFile, [TheVersion]$version) {
+		$versionjson = Get-Content $versionFile | ConvertFrom-Json
+		$versionjson.version = $version.Version
+		ConvertTo-Json $versionjson | Set-Content $versionFile
+	}
+
+	function Set-Version([string]$versionFile, [string]$newValue) {
+		$parsed = [TheVersion]::Parse("$newValue")
+		Write-Version $versionFile $parsed
+	}
+
+	function Bump-Version([string]$versionFile,
+		[bool]$bumpMajor, [bool] $bumpMinor,
+		[bool]$bumpPatch, [bool] $bumpBuild,
+		[string]$newValue = '')
+	{
+		$versionjson = Get-Content $versionFile | ConvertFrom-Json
+		$version = $versionjson.version
+		$parsed = [TheVersion]::Parse("$version")
+
+		if ($bumpMajor) {
+			if ($newValue -ne '') {
+				$newVersion = $parsed.SetMajor($newValue)
+			} else {
+				$newVersion = $parsed.bumpMajor()
+			}
+		} elseif ($bumpMinor) {
+			if ($newValue -ne '') {
+				$newVersion = $parsed.SetMinor($newValue)
+			} else {
+				$newVersion = $parsed.BumpMinor()
+			}
+		} elseif ($bumpPatch) {
+			if ($newValue -ne '') {
+				$newVersion = $parsed.SetPatch($newValue)
+			} else {
+				$newVersion = $parsed.BumpPatch()
+			}
+		} elseif ($bumpBuild) {
+			if ($newValue -ne '') {
+				$newVersion = $parsed.SetBuild($newValue)
+			} else {
+				$newVersion = $parsed.BumpBuild()
+			}
+		}
+
 		$versionjson.version = $newVersion.Version
 		ConvertTo-Json $versionjson | Set-Content $versionFile
 	}
 
-	$versionFile = "$rootDirectory\src\com.github.ui\version.json"
-	PatchVersionFile $versionFile $PatchVersion
+	if ($NewVersion -ne '' -and !($BumpMajor -or $BumpMinor -or $BumpPatch -or $BumpBuild)) {
+		$versionFile = "$rootDirectory\src\com.unity.git.ui\version.json"
+		Set-Version $versionFile $NewVersion
 
-	$versionFile = "$rootDirectory\src\git-for-unity\src\com.unity.git.ui\version.json"
-	PatchVersionFile $versionFile $PatchVersion
+		$versionFile = "$rootDirectory\src\com.unity.git.api\version.json"
+		Set-Version $versionFile $NewVersion
+	} else {
+		$versionFile = "$rootDirectory\src\com.unity.git.ui\version.json"
+		Bump-Version $versionFile $BumpMajor $BumpMinor $BumpPatch $BumpBuild $NewVersion
 
-	$versionFile = "$rootDirectory\src\git-for-unity\src\com.unity.git.api\version.json"
-	PatchVersionFile $versionFile $PatchVersion
+		$versionFile = "$rootDirectory\src\com.unity.git.api\version.json"
+		Bump-Version $versionFile $BumpMajor $BumpMinor $BumpPatch $BumpBuild $NewVersion
+	}
 
 }
